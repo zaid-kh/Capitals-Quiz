d7f6fa9fd76daafe62ecc231c344ea791e82a8d8
zaid-kh <z.kh2800@gmail.com>
2023-10-22 15:30:32 +0300

    base logic's variables and methods

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,2 +1,27 @@
 import { questions } from "./questions.js";
 console.log("questions: ", questions);
+
+const MAX_SCORE = questions.length;
+let score = 0;
+let currentQuestion = {
+  question: "Empty?",
+  options: ["Option", "Option", "Option", "Option"],
+  answer: "Answer",
+};
+console.log("currentQuestion: ", currentQuestion);
+
+/**
+ * retrieve a random question from questions array
+ * @returns question object
+ * @usecase `currentQuestion = getQuestion()`
+ */
+function getQuestion() {
+  // todo: eliminate recurrence
+  return questions[Math.floor(Math.random() * questions.length)];
+}
+
+function updateQuestionUI() {
+  // fill the placeholders of question with the current question
+}
+getQuestion();
+console.log("getQuestion(): ", getQuestion());
